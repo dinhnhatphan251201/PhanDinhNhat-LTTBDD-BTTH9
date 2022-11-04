b5ed022ebd3c5ae3efe4ced847c3fed560f19227
dinhnhatphan251201 <dinhnhatphan251201@gmail.com>
2022-11-04 10:52:09 +0700

    update

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useRef, useState } from "react";
+import { Animated, StyleSheet, Text, View } from "react-native";
+import { Button, Image } from "react-native";
+import icon from "./assets/donut_red.png";
+import Bai1 from "./Bai1";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Bai1 />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+    },
 });
